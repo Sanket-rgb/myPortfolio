8bb97e6a69f1a4728bae82b783c09dd25eae1c14
Sanket-rgb <67689067+Sanket-rgb@users.noreply.github.com>
2022-03-05 23:34:33 -0500

    Deploy

---
+++ src/data.js
@@ -0,0 +1,89 @@
+export const projects = [
+  {
+    title: "Joyn Social Media App",
+    subtitle: "Flutter and Dart",
+    description:
+      "Joyn, a social media mobile application with Blockchain and NFT technologies. It allows users to purchase cryptocurrency and to produce and sell NFTs.",
+    image: "./joyn.png",
+    link: "https://reactbootcamp.com",
+  },
+  {
+    title: "My Portfolio",
+    subtitle: "HTML, Tailwind CSS and ReactJS",
+    description:
+      "A developer portfolio built using languages like HTML, CSS and JavaScript. Used ReactJS as front-end and Netlify to push app on web with help of CDN(Content Delivery Network).",
+    image: "./project-4.gif",
+    link: "https://pythonbootcamp.com",
+  },
+
+  {
+    title: "Friendly Cars Dealership",
+    subtitle: "Oracle and MySQL",
+    description:
+      "Designed a relational database management system (RDBMS) using Oracle and MySQL for an online car sales platform, enabling the company to keep track of their customers’ purchasing information",
+    image: "./project-3.gif",
+    link: "https://jsbootcamp.com",
+  },
+  {
+    title: "PrimeID",
+    subtitle: "Flutter and Dart",
+    description:
+      "A mobile application using Flutter as a UI development kit to identify fake follow requests on social media platforms such as LinkedIn and Twitter.",
+    image: "./primeid.png",
+    link: "https://reedbarger.com",
+  },
+];
+
+export const skills = [
+  {
+    language: "Python",
+  },
+  {
+    language: "Objective-C",
+  },
+  {
+    language: "HTML",
+  },
+  {
+    language: "CSS",
+  },
+  {
+    language: "JavaScript",
+  },
+  {
+    language: "ReactJS",
+  },
+  {
+    language: "Core Java and J2EE",
+  },
+  {
+    language: "Flutter",
+  },
+  {
+    language: "Dart",
+  },
+  {
+    language: "Material UI",
+  },
+];
+
+export const testimonials = [
+  {
+    language: "Python",
+  },
+  {
+    language: "Objective-C",
+  },
+  {
+    language: "Java",
+  },
+  {
+    language: "HTML",
+  },
+  {
+    language: "CSS",
+  },
+  {
+    language: "JavaScript",
+  },
+];
